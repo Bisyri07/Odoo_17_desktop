45f45fa4112d58d710b82cb3cb8d123ceb1bb63d
bisyri 08 <mbisyri33@gmail.com>
2024-08-29 14:21:25 +0700

    Model testing CRUD - test create data

--- server/addons/estate/model_testing/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import e_p_tag_testing
--- server/addons/estate/model_testing/e_p_tag_testing.py
+++
@@ -1,8 +0,0 @@
-from odoo.tests import TransactionCase
-
-class estatePropertyTagTest(TransactionCase):
-
-    def setup(self):
-        super(estatePropertyTagTest).setUp()
-        self.estate_property_tag_model = self.env['estate.property.tag']
-    

3d0dba11f7d9e16991427588b30e47b9900487ce
bisyri 08 <mbisyri33@gmail.com>
2024-11-04 15:02:51 +0700

    Rest API configuration for sales order

--- server/addons/master_purchasing/controllers/controllers.py
+++ server/addons/master_purchasing/controllers/controllers.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from .token_based_auth import authenticate
+from odoo.http import request, Response
+from odoo import http
+import json
 
 
-# class MasterPurchasing(http.Controller):
-#     @http.route('/master_purchasing/master_purchasing', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class SalesorderApi(http.Controller):
+    # untuk mengakses record melalui autentikasi login akses
+    @http.route('/web/session/authenticate_', type='json', auth='none')
+    def authenticate_(self, db, login, password):
+        request.session.authenticate(db, login, password)
+        return request.env['ir.http'].session_info()
 
-#     @http.route('/master_purchasing/master_purchasing/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('master_purchasing.listing', {
-#             'root': '/master_purchasing/master_purchasing',
-#             'objects': http.request.env['master_purchasing.master_purchasing'].search([]),
-#         })
 
-#     @http.route('/master_purchasing/master_purchasing/objects/<model("master_purchasing.master_purchasing"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('master_purchasing.object', {
-#             'object': obj
-#         })
+    # untuk mendapatkan record
+    @http.route('/master_purchasing', auth='public')
+    @authenticate
+    def index(self, **kw):
+        sales = request.env['sales.order'].search([])
+        sales_order_list = []
+        for sale in sales:
+            sales_order_list.append({
+                'id': sale.id,
+                'item': sale.item.item,
+                'item_code': sale.item_code,
+            })
+        return Response(json.dumps(sales_order_list), content_type="application/json")
 
+        # another variation
+        # sales_order = http.request.env['sales.order'].search([])
+        # output = "<h1>Sales order</h1>"
+        
+        # for sale in sales_order:
+        #     output += '<li>' + str(sale['item']) + '</li>'
+
+        # output += '</ul>'
+        # return output

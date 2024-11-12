b20e79d12fff797c67e5806107acbcba36f477bd
bisyri 08 <mbisyri33@gmail.com>
2024-11-12 09:29:05 +0700

    Rendering Data Output JS (QWeb template)

--- server/addons/master_purchasing/static/src/js/public_widget.js
+++ server/addons/master_purchasing/static/src/js/public_widget.js
@@ -1,6 +1,7 @@
 /* @odoo-module */
-
 import publicWidget from "@web/legacy/js/public/public_widget";
+import { renderToElement } from "@web/core/utils/render"
+
 
 publicWidget.registry.jsTemplate = publicWidget.Widget.extend({
     selector: '.js_template',
@@ -8,5 +9,18 @@ publicWidget.registry.jsTemplate = publicWidget.Widget.extend({
     start(){
         // console.log("JS Template activated!")
         this.renderElement();
+    }
+})
+
+publicWidget.registry.templateWithVariables = publicWidget.Widget.extend({
+    selector: '.js_tmp_with_variables',
+    template: 'master_purchasing.templateWithVariables',
+    start(){
+        const content = renderToElement(this.template, {
+            string: "QWEB Tutorial using Javascript",
+            array: [1,2,3,4,5],
+            email: 'mbisyri33@gmail.com',
+        })
+        this.$target.html(content)
     }
 })

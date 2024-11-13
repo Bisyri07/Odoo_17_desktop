0baeea9db78634b5d5aa6863d1d373dc0ef924d4
bisyri 08 <mbisyri33@gmail.com>
2024-11-13 11:12:05 +0700

    Add counter event Js (QWeb template)

--- server/addons/master_purchasing/static/src/js/owl_component.js
+++ server/addons/master_purchasing/static/src/js/owl_component.js
@@ -1,23 +1,36 @@
 /* @odoo-module */
-import { Component, xml, mount, whenReady} from "@odoo/owl"
+import { Component, xml, mount, whenReady, useState } from "@odoo/owl"
+import { templates } from "@web/core/assets"
+
 
 class OwlMainComponent extends Component {
+    setup(){
+        this.state = useState({
+            counter: 0,
+        })
+    }
+
+    increaseCounter(){
+        this.state.counter++
+    }
 
 }
 
 // template
-OwlMainComponent.template = xml`
-<div class="p-4 border">
-    <h3>This is rendered using OWL</h3>
-</div>
-`
+// OwlMainComponent.template = xml`
+// <div class="p-4 border">
+//     <h3>This is rendered using OWL</h3>
+// </div>
+// `
+
+OwlMainComponent.template = "master_purchasing.owl_main_component";
 
 whenReady(
     ()=>{
         const element = document.querySelector('.js_template_owl')
         if (element){
         // mount is used to attach a component to a specific DOM element
-        mount(OwlMainComponent, element)
+        mount(OwlMainComponent, element, { templates })
         }
     }
 )

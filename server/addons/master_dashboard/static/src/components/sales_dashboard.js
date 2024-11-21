87cb8ce4e28d2ac19fc952a477cf790a4e025ff0
bisyri 08 <mbisyri33@gmail.com>
2024-11-21 13:40:18 +0700

    Initial KPI set up

--- server/addons/master_dashboard/static/src/components/sales_dashboard.js
+++ server/addons/master_dashboard/static/src/components/sales_dashboard.js
@@ -3,18 +3,19 @@
 
 // mengimpor registry dari modul inti Odoo
 import { registry } from "@web/core/registry"
+// import KpiCard dari kpi_card.js
+import { KpiCard } from "./kpi_card/kpi_card"
 
 // Component adalah library frontend untuk pembuatan UI di OWL 
 const { Component } = owl
 
 // export: Membuat kelas ini dapat diimpor di file lain
-export class OwlSalesDashboard extends Component {
+export class OwlSalesDashboard extends Component {}
 
-}
-
-// Baris ini menghubungkan komponen OwlSalesDashboard dengan template XML-nya.
-//ID template yang didefinisikan di file XML
+// Baris ini menghubungkan komponen OwlSalesDashboard dengan template XML-nya. ID template yang didefinisikan di file XML
 OwlSalesDashboard.template = "master_dashboard.OwlSalesDashboard" 
+OwlSalesDashboard.components = { KpiCard }
+
 
 // registry.category("actions") => mengakses kategori actions dalam registry
 // .add("master_dashboard.sales_dashboard", OwlSalesDashboard) menambahkan komponen OwlSalesDashboard 

5e013aef4f42d353ba6e7ffc074481d81a075d80
bisyri 08 <mbisyri33@gmail.com>
2024-11-22 05:38:02 +0700

    Move the setup content to the chart_renderer.js and add chart_renderer.js to sales_dashboard.js

---
+++ server/addons/master_dashboard/static/src/components/chart_renderer/chart_renderer.js
@@ -0,0 +1,70 @@
+/* @odoo-module */
+
+import { registry } from "@web/core/registry"
+import { loadJS } from "@web/core/assets"
+const { Component, onWillStart, useRef, onMounted } = owl
+
+
+export class ChartRenderer extends Component {
+    setup(){
+        this.chartRef = useRef("chart");
+        onWillStart(async ()=>{
+            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js")
+        });
+
+        onMounted(()=>this.renderChart())
+    }
+
+    renderChart(){
+        new Chart(
+        this.chartRef.el,
+        {
+          type: 'doughnut',
+          data: {
+              labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+              ],
+              datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(54, 162, 235)',
+                  'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+              },
+              {
+                label: 'My Second Dataset',
+                data: [100, 70, 150],
+                backgroundColor: [
+                  'red',
+                  'green',
+                  'orange'
+                ],
+                hoverOffset: 4
+              }
+              ]
+          },
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'bottom',
+              },
+              title: {
+                display: true,
+                text: 'Doughnut Chart',
+                position: 'bottom',
+              },
+            },
+          },
+        }
+        );
+    }
+}
+
+ChartRenderer.template = "master_dashboard.ChartRenderer"
+
--- server/addons/master_dashboard/static/src/components/sales_dashboard.js
+++ server/addons/master_dashboard/static/src/components/sales_dashboard.js
@@ -7,6 +7,8 @@ import { registry } from "@web/core/registry"
 import { KpiCard } from "./kpi_card/kpi_card"
 // Fungsi ini digunakan untuk memuat file JavaScript secara dinamis pada runtime
 import { loadJS } from "@web/core/assets"
+// import ChartRenderer dari chart_renderer.js
+import { ChartRenderer } from "./chart_renderer/chart_renderer"
 
 // Component adalah library frontend untuk pembuatan UI di OWL 
 const { Component, onWillStart, useRef, onMounted } = owl
@@ -14,46 +16,14 @@ const { Component, onWillStart, useRef, onMounted } = owl
 // export: Membuat kelas ini dapat diimpor di file lain
 export class OwlSalesDashboard extends Component {
     setup(){
-        // Referensi DOM elemen untuk chart
-        this.chartRef = useRef("chart");
-        // Memuat pustaka Chart.js sebelum komponen dimount
-        onWillStart(async ()=>{
-            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js")
-        });
 
-        onMounted(()=>{
-            const data = [
-            { year: 2010, count: 10 },
-            { year: 2011, count: 20 },
-            { year: 2012, count: 15 },
-            { year: 2013, count: 25 },
-            { year: 2014, count: 22 },
-            { year: 2015, count: 30 },
-            { year: 2016, count: 28 },
-          ];
-
-          new Chart(
-            this.chartRef.el,
-            {
-              type: 'bar',
-              data: {
-                labels: data.map(row => row.year),
-                datasets: [
-                  {
-                    label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
-                  }
-                ]
-              }
-            }
-          );
-        })
     }
 }
 
-// Baris ini menghubungkan komponen OwlSalesDashboard dengan template XML-nya. ID template yang didefinisikan di file XML
+// Baris ini menghubungkan komponen OwlSalesDashboard dengan template XML-nya.
+// ID template yang didefinisikan di file XML
 OwlSalesDashboard.template = "master_dashboard.OwlSalesDashboard" 
-OwlSalesDashboard.components = { KpiCard }
+OwlSalesDashboard.components = { KpiCard, ChartRenderer }
 
 
 // registry.category("actions") => mengakses kategori actions dalam registry

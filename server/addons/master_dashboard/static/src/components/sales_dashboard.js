d36b4a8cf361ee7fe93d80396b2af38e6ffa5415
bisyri 08 <mbisyri33@gmail.com>
2024-11-23 16:27:54 +0700

    Dynamically change quotation KPI value

--- server/addons/master_dashboard/static/src/components/sales_dashboard.js
+++ server/addons/master_dashboard/static/src/components/sales_dashboard.js
@@ -9,15 +9,42 @@ import { KpiCard } from "./kpi_card/kpi_card"
 import { loadJS } from "@web/core/assets"
 // import ChartRenderer dari chart_renderer.js
 import { ChartRenderer } from "./chart_renderer/chart_renderer"
+// 
+import { useService } from "@web/core/utils/hooks"
+
 
 // Component adalah library frontend untuk pembuatan UI di OWL 
-const { Component, onWillStart, useRef, onMounted } = owl
+const { Component, onWillStart, useRef, onMounted, useState } = owl
 
 // export: Membuat kelas ini dapat diimpor di file lain
 export class OwlSalesDashboard extends Component {
-    setup(){
 
+    // 1. Inisialisasi properti dan hook dalam komponen Owl.
+    setup(){
+        this.state = useState({
+            quotations: {
+                value: 10,
+                percentage: 6,
+            },
+            period:90,
+        })
+        
+        // 2. fungsi hook owl untuk mengakses ORM service dan disimpan `this.orm` 
+        // agar bisa digunakan di fungsi lain 
+        this.orm = useService("orm")
+
+        // 4. Memanggil fungsi getQuotations sebelum komponen ditampilkan di layar.
+        onWillStart(async ()=> {
+            await this.getQuotations()
+        })
     }
+
+    // 3. digunakan untuk mengambil data dari server menggunakan ORM
+    async getQuotations(){
+        const data = await this.orm.searchCount("sale.order", [['state', 'in', ['sent', 'draft']]])
+        this.state.quotations.value = data
+    }
+
 }
 
 // Baris ini menghubungkan komponen OwlSalesDashboard dengan template XML-nya.

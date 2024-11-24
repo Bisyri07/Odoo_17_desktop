eaba58395792c0a7e366721bc393d8c7c2a340e7
bisyri 08 <mbisyri33@gmail.com>
2024-11-24 11:49:24 +0700

    Dynamically change quotation KPI value from sale.order model

--- server/addons/master_dashboard/static/src/components/sales_dashboard.js
+++ server/addons/master_dashboard/static/src/components/sales_dashboard.js
@@ -9,9 +9,8 @@ import { KpiCard } from "./kpi_card/kpi_card"
 import { loadJS } from "@web/core/assets"
 // import ChartRenderer dari chart_renderer.js
 import { ChartRenderer } from "./chart_renderer/chart_renderer"
-// 
+// digunakan untuk mendapatkan referensi ke suatu service dalam komponen Owl
 import { useService } from "@web/core/utils/hooks"
-
 
 // Component adalah library frontend untuk pembuatan UI di OWL 
 const { Component, onWillStart, useRef, onMounted, useState } = owl
@@ -27,6 +26,7 @@ export class OwlSalesDashboard extends Component {
                 percentage: 6,
             },
             period:90,
+
         })
         
         // 2. fungsi hook owl untuk mengakses ORM service dan disimpan `this.orm` 
@@ -35,14 +35,41 @@ export class OwlSalesDashboard extends Component {
 
         // 4. Memanggil fungsi getQuotations sebelum komponen ditampilkan di layar.
         onWillStart(async ()=> {
+            // memanggil pustaka manipulasi tanggal dari moment.min.js
+            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js")
+
+            // memanggil data tanggal sebelum dirender ke halaman
+            await this.getDates()
+
+            // memanggil data quotation sebelum dirender ke halaman
             await this.getQuotations()
+
         })
     }
 
     // 3. digunakan untuk mengambil data dari server menggunakan ORM
     async getQuotations(){
-        const data = await this.orm.searchCount("sale.order", [['state', 'in', ['sent', 'draft']]])
-        this.state.quotations.value = data
+        let domain = [['state', 'in', ['sent', 'draft']]]
+
+        // jika semua state.period tidak lebih dari 0 maka akan menghitung semua record sale.order
+        if (this.state.period > 0) {
+            domain.push(['date_order', '>', this.state.date])
+        }
+
+        const data = await this.orm.searchCount("sale.order", domain)
+
+        this.state.quotations.value = data 
+    }
+
+    // 5. digunakan untuk mendapatkan quotation dengan cara merubah tanggalnya terlebih dahulu
+    async onChangePeriod(){
+        await this.getDates()
+        await this.getQuotations()
+    } 
+
+    // 6. digunakan untuk mendapatkan data tanggal hari ini yang dikurangi tanggal period
+    async getDates(){
+        this.state.date = moment().subtract(this.state.period, 'days').format('L')
     }
 
 }

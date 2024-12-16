f0be693629c0fb24e12ffeec0e645d715985d76c
bisyri 08 <mbisyri33@gmail.com>
2024-12-16 11:16:16 +0700

    Inherit 2 models from ir.ui.view and ir.actions.act_window.view

---
+++ server/addons/map/models/ir_actions.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class WindowActionView(models.Model):
+    _inherit = 'ir.actions.act_window.view'
+
+
+    view_mode = fields.Selection(
+        selection_add=[
+            ('lmap', 'Leaflet Map'),
+        ],
+        ondelete = {'lmap': 'cascade'}
+    )
